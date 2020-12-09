d37a689fae7b0fa33214012d3f1f092dfb59e057
Yilong <yyu0995@conestogac.on.ca>
2020-12-09 17:01:06 -0500

    Added result/score, next button

--- app/src/main/java/ca/thewizards/flashcards/PlayActivity.java
+++ app/src/main/java/ca/thewizards/flashcards/PlayActivity.java
@@ -26,12 +26,14 @@ public class PlayActivity extends AppCompatActivity {
     private List<Question> questions;
     private TextInputEditText txt_answer;
 
-    private ImageView imageViewFace;
+    private ImageView image_Face;
     private Button btn_ok;
+    private TextView view_Result;
+    private Button btn_next;
 
     private boolean animation;
     private int collectionId;
-    private int curQuestion = 0;
+    private int questionIndex = 0;
     private CardView cardView;
     private TextView txt_question;
 
@@ -61,76 +63,100 @@ public class PlayActivity extends AppCompatActivity {
         application = (FlashcardsApplication)getApplication();
         questions = application.getQuestions(collectionId);
 
-        // Test
-        Question q1 = new Question(1, "red", "What color is apple?", 1);
-        Question q2 = new Question(2, "yellow", "What color is banana?", 1);
+        // Test; TODO: remove later
+        Question q1 = new Question(1, "Red", "What color is apple?", 1);
+        Question q2 = new Question(2, "Yellow", "What color is banana?", 1);
+        Question q3 = new Question(3, "Blue", "What color is sky?", 1);
         questions.add(q1);
         questions.add(q2);
+        questions.add(q3);
 
         totalQuestion = questions.size();
+        totalCorrect = 0;
 
         if (totalQuestion > 0){
             txt_answer = findViewById(R.id.txt_answer);
             txt_question = findViewById(R.id.txt_question);
 
-            imageViewFace = findViewById(R.id.image_Face);
+            image_Face = findViewById(R.id.image_Face);
             btn_ok = findViewById(R.id.btn_ok);
             cardView = findViewById(R.id.question_card_view);
+            btn_next = findViewById(R.id.btn_next);
+            view_Result = findViewById(R.id.view_result);
+            view_Result.setText("Result: " + totalCorrect + "/" + totalQuestion);
 
             btn_ok.setOnClickListener(handleClick());
-            txt_question.setText(questions.get(curQuestion).getQuestion());
+            btn_next.setOnClickListener(handleClick());
+            displayQuestion(txt_question, questionIndex);
         }
+    }
+
+    private void displayQuestion(TextView txt, int questionIndex){
+        int questionNo = questionIndex + 1;
+        txt.setText("Q" + questionNo + ": " + questions.get(questionIndex).getQuestion());
     }
 
     View.OnClickListener handleClick(){
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PlayActivity.this, "button clicked", Toast.LENGTH_SHORT).show();
-
-                // Animation effect
-                if (animation){
-                    cardView.animate().withLayer()
+            switch (v.getId()){
+                case R.id.btn_ok:
+                    // Animation effect
+                    if (animation){
+                        cardView.animate().withLayer()
                             .rotationY(90)
                             .setDuration(300)
                             .withEndAction(
-                                    new Runnable() {
-                                        @Override public void run() {
-                                            txt_question.setText(questions.get(curQuestion).getAnswer());
+                                new Runnable() {
+                                    @Override public void run() {
+                                    txt_question.setText("Q" + (questionIndex + 1) + ": " +
+                                            questions.get(questionIndex).getAnswer());
 
-                                            // second quarter turn
-                                            cardView.setRotationY(-90);
-                                            cardView.animate().withLayer()
-                                                    .rotationY(0)
-                                                    .setDuration(1000)
-                                                    .start();
-                                        }
+                                    // second quarter turn
+                                    cardView.setRotationY(-90);
+                                    cardView.animate().withLayer()
+                                            .rotationY(0)
+                                            .setDuration(1000)
+                                            .start();
                                     }
+                                }
                             ).start();
-                }
-                else {
-                    txt_question.setText(questions.get(curQuestion).getAnswer());
-                }
+                    }
+                    else {
+                        txt_question.setText("Q" + (questionIndex + 1) + ": " +
+                                questions.get(questionIndex).getAnswer());
+                    }
 
-                // happy face and sad face
-                String answerInput = txt_answer.getText().toString().toLowerCase();
+                    // happy face and sad face
+                    String answerInput = txt_answer.getText().toString().toLowerCase();
+                    String answerCorrect = questions.get(questionIndex).getAnswer().toLowerCase();
 
-                // TODO: check correct answer from database
-                String answerCorrect = "right".toLowerCase(); // test
+                    if(answerCorrect.equals(answerInput)){
+                        image_Face.setImageResource(R.drawable.ic_happy_face);
 
-                Toast.makeText(PlayActivity.this, answerInput + "=" + answerCorrect, Toast.LENGTH_SHORT).show();
+                        totalCorrect++;
+                    }
+                    else{
+                        image_Face.setImageResource(R.drawable.ic_sad_face);
+                    }
 
-                if(answerCorrect.equals(answerInput)){
-                    imageViewFace.setImageResource(R.drawable.ic_happy_face);
-                }
-                else{
-                    imageViewFace.setImageResource(R.drawable.ic_sad_face);
-                }
+                    // display the result
+                    view_Result.setText("Result: " + totalCorrect + "/" + totalQuestion);
 
+                    // show btn_next
+                    if(totalQuestion != (questionIndex + 1)){
+                        btn_next.setVisibility(View.VISIBLE);
+                    }
+                    questionIndex++;
 
-                // display the result
-
-                // skip to next question
+                    break;
+                case R.id.btn_next:
+                    txt_answer.setText("");
+                    btn_next.setVisibility(View.INVISIBLE);
+                    displayQuestion(txt_question, questionIndex);
+                    break;
+            }
             }
         };
     }
